53c5ce25a8d8e6a13380386148cccd49a49958d7
maguodong <maguodong@nicetuan.com>
2021-07-19 14:40:24 +0800

    add redis dependency

---
+++ src/test/java/com/basic/tms/RedisTest.java
@@ -0,0 +1,24 @@
+package com.basic.tms;
+
+import mockit.Injectable;
+import mockit.Mocked;
+import mockit.integration.junit4.JMockit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@RunWith(JMockit.class)
+public class RedisTest {
+
+    @Mocked
+    @Injectable
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @Test
+    public void testAdd(){
+        Long add = stringRedisTemplate.opsForSet().add("1", "111");
+        System.out.println(add);
+
+    }
+}
